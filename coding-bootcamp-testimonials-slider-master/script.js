be54b9ef2d818ba6df443c02017f6a65f375a914
Dinesh <baladnes@gmail.com>
2020-05-18 20:48:51 +0545

    Initial Commit

---
+++ coding-bootcamp-testimonials-slider-master/script.js
@@ -0,0 +1,21 @@
+// Reference : w3schoold
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  const slides = document.getElementsByClassName("slide");
+  
+  if (n > slides.length) slideIndex = 1
+  if (n < 1) slideIndex = slides.length
+
+  for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
